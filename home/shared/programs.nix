a4b934246382090195b4dd6d71e13cbf22dd6aa8
Foxocube <git@foxocube.xyz>
2025-05-04 19:43:44 +0100

    Disable LD in home-manager to fix build

--- home/shared/programs.nix
+++ home/shared/programs.nix
@@ -34,13 +34,13 @@ let
       #  enable = true;
       #};
 
-      nix-ld.enable = true;
-      nix-ld.libraries = with pkgs; [
-        # Add any missing dynamic libraries for unpackaged programs
-        # here, NOT in environment.systemPackages
-        icu
-        ncurses6
-      ];
+      #nix-ld.enable = true;
+      #nix-ld.libraries = with pkgs; [
+      #  # Add any missing dynamic libraries for unpackaged programs
+      #  # here, NOT in environment.systemPackages
+      #  icu
+      #  ncurses6
+      #];
     };
   };
 in
