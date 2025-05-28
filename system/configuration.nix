d80db5e7bfb33db08421924816dc0b62e3f8fa8a
Foxocube <git@foxocube.xyz>
2025-05-28 17:35:18 +0100

    Add cache

--- system/configuration.nix
+++ system/configuration.nix
@@ -13,6 +13,16 @@
       keep-outputs = true
       keep-derivations = true
     '';
+    settings = {
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://channable-public.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 
   # Virtualization settings
