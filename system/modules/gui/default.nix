701faa0dd3cdbf5065264a0555e6b443473900aa
Foxocube <git@foxocube.xyz>
2025-08-11 15:46:22 +0100

    Set up greeter

--- system/modules/gui/default.nix
+++ system/modules/gui/default.nix
@@ -8,15 +8,23 @@
     ddcutil
     corefonts
     discord
+    greetd.tuigreet
   ];
 
   programs.hyprland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
+  services.greetd = {
+    enable = true;
+    settings.default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+  };
+
   programs.firefox.enable = true;
 
   boot.plymouth.enable = true;
-  boot.plymouth.theme = "breeze";
+  boot.plymouth.font = "${pkgs.nerd-fonts.fira-code}/share/fonts/truetype/NerdFonts/FiraCode/FiraCodeNerdFont-Regular.ttf";
+  boot.plymouth.logo = "${pkgs.nixos-icons}/share/icons/hicolor/128x128/apps/nix-snowflake.png";
+#  boot.plymouth.theme = "breeze";
 
   services.geoclue2.enable = true;
 }
