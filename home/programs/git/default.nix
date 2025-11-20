32c99028f14d9091ae55b11fa61dfb56c19d4826
Sam M <sam.m@saluscyber.com>
2025-11-20 11:44:34 +0000

    Extract work-specific git config

--- home/programs/git/default.nix
+++ home/programs/git/default.nix
@@ -1,50 +1,81 @@
-_: let
-  gitConfig = {
-    init.defaultBranch = "main";
-    pull.rebase = false;
-    push.autoSetupRemote = true;
-    push.default = "tracking";
-    color.ui = true;
-    core.askPass = ""; # needs to be empty to use terminal for ask pass
-    credential.helper = "store"; # want to make this more secure
-    pager.branch = false;
-    commit.gpgsign = true;
+{
+  config,
+  lib,
+  ...
+}: let
+  gitExtraConfig =
+    {
+      # Shared config
+      init.defaultBranch = "main";
+      pull.rebase = false;
+      push.autoSetupRemote = true;
+      push.default = "tracking";
+      color.ui = true;
+      core.askPass = ""; # needs to be empty to use terminal for ask pass
+      pager.branch = false;
 
-    credential = {
-      "https://dev.azure.com" = {
-        useHttpPath = true;
+      credential = {
+        helper = "store"; # want to make this more secure
+
+        "https://dev.azure.com" = {
+          useHttpPath = true;
+        };
       };
-    };
+    }
+    // (
+      if !config.defaultGit.work
+      then {
+        # Home-specific config
+        gpg = {
+          format = "ssh";
+        };
 
-    gpg = {
-      format = "ssh";
+        commit.gpgsign = true;
+      }
+      else {}
+    );
+
+  gitConfig =
+    {
+      # Shared config
+      enable = true;
+      lfs.enable = true;
+      extraConfig = gitExtraConfig;
+    }
+    // (
+      if !config.defaultGit.work
+      then {
+        # Home config
+        userName = "Foxocube";
+        userEmail = "git@foxocube.xyz";
+        signing = {
+          key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFygf49qzrMruoAeB/Y0RcpkTFGpTVpRr+bwRhDQIZzI";
+          signByDefault = true;
+        };
+      }
+      else {}
+    );
+in {
+  options = {
+    defaultGit = {
+      work = lib.mkEnableOption "Work Git configuration";
     };
   };
-in {
-  programs = {
-    git = {
-      enable = true;
 
-      #TODO: Work config
-      userName = "Foxocube";
-      userEmail = "git@foxocube.xyz";
-      signing = {
-        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFygf49qzrMruoAeB/Y0RcpkTFGpTVpRr+bwRhDQIZzI";
-        signByDefault = true;
-      };
-      extraConfig = gitConfig;
-      lfs.enable = true;
-      #delta.enable = true;
-      #diff-highlight.enable = true;
-      #diff-so-fancy.enable = true;
-      #difftastic.enable = true;
-    };
-    gh = {
-      enable = true;
-      gitCredentialHelper = {
+  config = {
+    imports =
+      if config.defaultGit.work
+      then [./work.nix]
+      else [];
+    programs = {
+      git = gitConfig;
+      gh = {
         enable = true;
+        gitCredentialHelper = {
+          enable = true;
+        };
+        settings.git_protocol = "ssh";
       };
-      settings.git_protocol = "ssh";
     };
   };
 }
