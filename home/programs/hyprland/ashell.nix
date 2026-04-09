3ef298e0aaf6d222d183497a20ac7c652c211347
Foxocube <git@foxocube.xyz>
2026-04-09 23:42:09 +0100

    Add bits to ashell

--- home/programs/hyprland/ashell.nix
+++ home/programs/hyprland/ashell.nix
@@ -6,11 +6,13 @@
   marvin-info = pkgs.writeShellScript "marvin-info.sh" ''
     TOKEN=$(cat /run/secrets/MARVIN)
 
-    #echo -n "󰚩 "
-    ${lib.getExe pkgs.curl} -s \
-        -H "X-API-Token: $TOKEN" \
-        http://localhost:12082/api/todayItems \
-      | ${lib.getExe pkgs.jq} -c ".[0] | { text: .title, alt: .title }"
+    while true; do
+      ${lib.getExe pkgs.curl} -s \
+          -H "X-API-Token: $TOKEN" \
+          http://localhost:12082/api/todayItems \
+        | ${lib.getExe pkgs.jq} -c ".[0] | { text: .title, alt: .title }"
+      ${lib.getExe' pkgs.coreutils "sleep"} 60
+    done
   '';
 
   marvin-open = pkgs.writeShellScript "marvin-open.sh" ''
@@ -24,6 +26,14 @@
         ${lib.getExe pkgs.jq} -c "\"https://app.amazingmarvin.com/#t=\"+.[0]._id" -r
       )
   '';
+
+  brightness-get = pkgs.writeShellScript "brightness.sh" ''
+    while true; do
+      ${lib.getExe pkgs.brightnessctl} -m | ${lib.getExe' pkgs.coreutils "head"} -n 1 | ${lib.getExe pkgs.gawk} -F',' '{print $4}' | ${lib.getExe pkgs.jq} -R -c "{ text: ., alt: . }"
+
+      ${lib.getExe' pkgs.coreutils "sleep"} 1
+    done
+  '';
 in {
   programs.ashell = {
     enable = true;
@@ -33,7 +43,7 @@ in {
       modules = {
         left = ["Workspaces" "Marvin"]; # "Marvin"
         center = ["WindowTitle"];
-        right = ["SystemInfo" ["Clock" "Privacy" "Settings"] "Tray"]; # "Tray"
+        right = ["SystemInfo" "Backlight" ["Clock" "Privacy" "Settings"] "Tray"]; # "Tray"
       };
 
       workspaces = {
@@ -47,7 +57,6 @@ in {
           "DownloadSpeed"
           "UploadSpeed"
           "Temperature"
-          #backlight
 
           #battery
           {Disk = "/home";}
@@ -71,6 +80,24 @@ in {
 
           listen_cmd = "${marvin-info}";
           command = "${marvin-open}";
+        }
+        {
+          name = "Backlight";
+          type = "Text";
+          command = "${brightness-get}";
+          listen_cmd = "${brightness-get}";
+
+          icons = {
+            "^(\d|10|11)%$" = "";  #  0-11
+            "^(1[2-9]|2[0-2])%$" = "";  # 12-22
+            "^(2[3-9]|3[0-3])%$" = "";  # 23-33
+            "^(3[4-9]|4[0-4])%$" = "";  # 34-44
+            "^(4[5-9]|5[0-5])%$" = "";  # 45-55
+            "^(5[6-9]|6[0-6])%$" = "";  # 56-66
+            "^(6[7-9]|7[0-7])%$" = "";  # 67-77
+            "^(7[8-9]|8[0-8])%$" = "";  # 78-88
+            "^(89|9\d|100)%$" = "";  # 89-100
+          };
         }
       ];
 
