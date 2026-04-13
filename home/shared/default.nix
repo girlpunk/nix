a88ea1aa44dc3cd689baeae4e3070c283fc09ac6
Foxocube <git@foxocube.xyz>
2026-04-13 18:21:28 +0100

    Nano configuration

--- home/shared/default.nix
+++ home/shared/default.nix
@@ -106,6 +106,26 @@ in {
     #    sessionPath = [
     #      "$HOME/.dotnet/tools"
     #    ];
+
+    file.nanorc = {
+      text = ''
+        ## Interpret digits given on the command line after a colon after a filename
+        ## as the line number to go to in that file.
+        set colonparsing
+
+        ## Remember the used search/replace strings for the next session.
+        set historylog
+
+        ## Enable vim-style lock-files.  This is just to let a vim user know you
+        ## are editing a file [s]he is trying to edit and vice versa.  There are
+        ## no plans to implement vim-style undo state in these files.
+        set locking
+
+        ## Use the end of the title bar for some state flags: I = auto-indenting,
+        ## M = mark, L = hard-wrapping long lines, R = recording, S = soft-wrapping.
+        set stateflags
+      '';
+    };
   };
 
   programs.nh = {
