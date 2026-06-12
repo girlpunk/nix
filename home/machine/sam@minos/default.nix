1555d22d16f41c3b85a6de1507ea93433ff66f52
Foxocube <git@foxocube.xyz>
2026-06-12 20:51:40 +0100

    Start android dev stuff

--- home/machine/sam@minos/default.nix
+++ home/machine/sam@minos/default.nix
@@ -1,2 +1,7 @@
 _: {
+  imports = [
+    ../../programs/activitywatch
+    ../../programs/vscode
+    ../../programs/android
+  ];
 }
---
+++ system/programs/android/default.nix
@@ -0,0 +1,32 @@
+{pkgs, ...}:
+let
+  androidEnv = pkgs.androidenv.override {licenseAccepted = true;};
+
+  # Versions are from these two
+  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/mobile/androidenv/compose-android-packages.nix
+  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/mobile/androidenv/repo.json
+  # Remember to check vresions from the right branch
+  androidComposition = androidEnv.composeAndroidPackages {
+    cmdLineToolsVersion = "19.0";
+    toolsVersion = "26.1.1";
+    platformToolsVersion = "36.0.0";
+    buildToolsVersions = [
+      "36.0.0"
+      "35.0.0"
+      "34.0.0"
+    ];
+    platformVersions = ["36" "34"];
+    includeSources = true;
+
+    useGoogleAPIs = true;
+
+    #includeExtras = ["extras;google;gcm"];
+  };
+in
+{
+  environment.systemPackages = with pkgs; [
+    (android-studio.withSdk androidComposition.androidsdk)
+    androidComposition.androidsdk
+    androidComposition.platform-tools
+  ];
+}
