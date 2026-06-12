64d14fab2c73b25a71d568a60b2a9bf63a9d85a4
Foxocube <git@foxocube.xyz>
2026-06-12 20:40:44 +0100

    Add cnpg autocomplete

--- system/programs/kubernetes-client.nix
+++ system/programs/kubernetes-client.nix
@@ -1,6 +1,15 @@
-{pkgs, ...}: {
+{pkgs, ...}:
+let
+  cnpgCompletions = pkgs.writeShellScriptBin "kubectl_complete-cnpg" ''
+    # Call the __complete command passing it all arguments
+    kubectl cnpg __complete "\$@"
+  '';
+
+in
+{
   environment.systemPackages = with pkgs; [
     kubectl
     kubectl-cnpg
+    cnpgCompletions
   ];
 }
